a5e1258e1c94e319120de7080dbbfa0c12bdd3c2
shaunhossain <shaunhossain95@gmail.com>
2023-06-18 17:22:38 +0600

    remove barikoi trace url

--- app/src/main/java/com/shaunhossain/traceservice/utils/Constants.kt
+++ app/src/main/java/com/shaunhossain/traceservice/utils/Constants.kt
@@ -3,5 +3,5 @@ package com.shaunhossain.traceservice.utils
 object Constants {
     const val TRACKER_LOCATION_TABLE: String = "TrackerLocation"
     const val TRACKER_LOCATION_DATABASE: String = "TrackerLocationDatabase"
-    const val BASE_URL_TRACE_SERVER: String = "https://trace.bmapsbd.com"
+    const val BASE_URL_TRACE_SERVER: String = "barikoi trace server"
 }
